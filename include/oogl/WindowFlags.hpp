1574a8d769c360adc3e7550e8722bb153623a2a9
Overv <overv161@gmail.com>
2010-12-23 04:42:26 +0100

    Set up project files and created GL::Window class, GL::WindowFlags enumeration and a window creation sample.
    - Added project files for Visual C++ 2008 and 2010.
    
    - Created and implemented GL::Window on Windows.
    
    - Added a window creation example with project files for VS2008 and VS2010.

---
+++ include/oogl/WindowFlags.hpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////
+// 
+// Copyright (c) 2010, Alexander Overvoorde. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+// 	notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// 	notice, this list of conditions and the following disclaimer in the
+// 	documentation and/or other materials provided with the distribution.
+// 3. All advertising materials mentioning features or use of this software
+// 	must display the following acknowledgement:
+// 	This product includes software developed by Overv Projects.
+// 4. Neither the name of Overv Projects nor the
+// 	names of its contributors may be used to endorse or promote products
+// 	derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY ALEXANDER OVERVOORDE ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL ALEXANDER OVERVOORDE BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+////////////////////////////////////////////////////////////
+
+#pragma once
+
+#ifndef OOGL_WINDOWFLAGS_HPP
+#define OOGL_WINDOWFLAGS_HPP
+
+namespace GL
+{
+	////////////////////////////////////////////////////////////
+	// Enumeration of window properties.
+	////////////////////////////////////////////////////////////
+
+	namespace WindowFlags
+	{
+		enum
+		{
+			CenterScreen = 1,
+			Minimized = 1 << 1,
+			Maximized = 1 << 2,
+			Caption = 1 << 3,
+			Sizable = 1 << 4
+		};
+	}
+}
+
+#endif
---
+++ samples/simplewindow/simplewindow.cpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include <oogl/Window.hpp>
+
+int main()
+{
+	GL::Window window( 800, 600, 0, 0, "OpenGL Window", GL::WindowFlags::Caption | GL::WindowFlags::CenterScreen );
+
+	while ( window.IsOpen() )
+	{
+		window.GetEvents();
+	}
+}
